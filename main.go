6c194119997b3b84f0ff700c8062011977f13b27
spavels2 <spavels2@mail.ru>
2024-08-29 20:47:03 +0800

    name.go  abgrade test

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	var word1, word2, word3, word4 = "имя", "твое", "мне", "знакомо"
+
+	fmt.Println(word4, word3, word2, word1)
+	fmt.Println(word3, word1, word4, word3)
+	fmt.Println(word2, word4, word1, word3)
 }
